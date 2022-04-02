49c45c3b69c17eaffbf8f061ba6af5129155d782
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-02 02:29:30 -0500

    added default value to url

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,18 +1,22 @@
-function Product({company, colors, featured, price, name, image}) {
-    const {url, width, height} = image[0]
-    const productStyle = {
-        width: width,
-        height: height
-    }
-    return (
-        <div>
-            <h2>Company name: {company}</h2>
-            <img src={url} alt={name} style={productStyle}/>
-            <p>{name}</p>
-            <p>Available colors: {colors}</p>
-            <p>${price}</p>
-            {featured  && <p>FEATURED!</p>}
-        </div>
-    )
+function Product({ company, colors, featured, price, name, image }) {
+  const { // get default image if no image is found
+    url = "https://images.unsplash.com/photo-1628155930542-3c7a64e2c833?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    width,
+    height,
+  } = image[0];
+  const productStyle = {
+    width: width,
+    height: height,
+  };
+  return (
+    <div>
+      <h2>Company name: {company}</h2>
+      <img src={url} alt={name} style={productStyle} />
+      <p>{name}</p>
+      <p>Available colors: {colors}</p>
+      <p>${price}</p>
+      {featured && <p>FEATURED!</p>}
+    </div>
+  );
 }
 export default Product;
