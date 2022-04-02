67654178ca494d26c37299206986c46d6616ef56
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-01 20:46:03 -0500

    added button with svg

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,12 +1,27 @@
 import SidebarItem from './SidebarItem'
+import SidebarSocialItem from './SidebarSocialItem'
 import {links, social} from "./links"
-
+import xMark from "./xMark.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faXmark
+  } from "@fortawesome/free-solid-svg-icons";
 function Sidebar() {
 
     return <>
-        {links.map(link => {
-            return <SidebarItem key={link.id} {...link} />
-        })}
+    <aside>
+    <button><img src={xMark} alt="close mark" /></button>  
+        <div>
+            {links.map(link => {
+                return <SidebarItem key={link.id} {...link} />
+            })}
+        </div>
+        <div>
+            {social.map(item => {
+                return <SidebarSocialItem key={item.id} {...item} />
+            })}
+        </div>
+    </aside>
     </>
 }
 
